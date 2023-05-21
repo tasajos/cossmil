54f36757216ab38fbf5f796463f480738731a887
tasajos <tasajos@gmail.com>
2023-05-21 19:07:21 -0400

    update cierre de caja

--- y/src/app/Components/Contabilidad/cajachicacierre/cajachicacierre.component.ts
+++ y/src/app/Components/Contabilidad/cajachicacierre/cajachicacierre.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cajachicacierre',
   templateUrl: './cajachicacierre.component.html',
   styleUrls: ['./cajachicacierre.component.css']
 })
-export class CajachicacierreComponent {
+export class CajachicacierreComponent implements AfterViewInit {
+  @ViewChild('inputPassword2', { static: false }) cajaChicaInput!: ElementRef<HTMLInputElement>;
 
+  ngAfterViewInit() {
+    const cajaChicaInput = this.cajaChicaInput.nativeElement;
+
+    const btns = document.querySelectorAll('.btn');
+    btns.forEach((btn: any) => {
+      btn.addEventListener('click', () => {
+        if (!btn.classList.contains('btn-primary')) { // Excluir botón "Cerrar Caja"
+          const value = btn.innerText;
+          cajaChicaInput.value += value;
+        }
+      });
+    });
+  }
+  borrarCampo() {
+    this.cajaChicaInput.nativeElement.value = '';
+  }
 }
