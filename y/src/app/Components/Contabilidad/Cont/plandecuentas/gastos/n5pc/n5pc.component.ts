52253fb9bd32320661fe10ab1844ad3d8521e929
tasajos <tasajos@gmail.com>
2023-06-16 09:54:48 -0400

    cp 5 update

--- y/src/app/Components/Contabilidad/Cont/plandecuentas/gastos/n5pc/n5pc.component.ts
+++ y/src/app/Components/Contabilidad/Cont/plandecuentas/gastos/n5pc/n5pc.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ContabiliadService } from 'src/app/services/contabiliad.service';
+import { HttpClient } from '@angular/common/http';
+import { CProductosIntern4,CProductosIntern5 } from 'src/app/Interfaz/contabilidad';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-n5pc',
   templateUrl: './n5pc.component.html',
   styleUrls: ['./n5pc.component.css']
 })
-export class N5pcComponent {
+export class N5pcComponent implements OnInit {
+  formulario: FormGroup;
+  activos: CProductosIntern4[] = [];
+  filteredActivos: CProductosIntern4[] = []; // Lista filtrada de activos
 
-}
+  constructor(
+    private fb: FormBuilder,
+    private _snackBar: MatSnackBar,
+    private _rcontabilidadchicaService: ContabiliadService,
+    private router: Router,
+    private http: HttpClient,
+    private route: ActivatedRoute
+  ) {
+    this.formulario = this.fb.group({
+      nivel: ['', Validators.required],
+      nombrecuenta: ['', Validators.required],
+      cuentamayor: ['', Validators.required],
+      totalnum: ['', Validators.required],
+      numero: ['', Validators.required],
+         });
+  }
+
+  registrarcuenta() {
+
+     const rcactivo: CProductosIntern5 = {
+      nivel: this.formulario.value.nivel,
+      nombrecuenta: this.formulario.value.nombrecuenta,
+      cuentamayor: this.formulario.value.cuentamayor,
+      totalnum: this.formulario.value.totalnum,
+      numero: this.formulario.value.numero,
+     
+    };
+
+    // Enviamos objeto al backend
+    this._rcontabilidadchicaService.postcuentaproducto5(rcactivo).subscribe(_data => {
+      this.mensajeExito('registrado');
+      location.reload();
+      this.router.navigate(['/ppasivo5']);
+    });
+  }
+
+  mensajeExito(texto: string) {
+    this._snackBar.open(`El proceso fue realizado y ${texto} con exito`, '', {
+      duration: 2000,
+      horizontalPosition: 'right',
+    });
+  }
+
+  ngOnInit(): void {
+   // Lógica del campo
+   this.formulario.get('nivel')?.valueChanges.subscribe(nivelSeleccionado => {
+    let cuentamayor = 0;
+    switch (nivelSeleccionado) {
+           case '5':
+        cuentamayor = 4;
+        break;
+      case '6':
+        cuentamayor = 5;
+        break;
+    }
+    this.formulario.patchValue({ cuentamayor });
+
+    // Filtrar activos
+    if (cuentamayor === 2) {
+      this.filteredActivos = this.activos.filter(activo => activo.nivel === 3);
+    } else {
+      this.filteredActivos = this.activos;
+    }
+  });
+
+  this._rcontabilidadchicaService.getcuentaproducto4().subscribe(
+    (activos: CProductosIntern4[]) => {
+      this.activos = activos;
+      this.filteredActivos = activos; // Inicialmente, mostrar todos los activos
+    },
+    (error) => {
+      console.error('Error al obtener los datos de getactivo():', error);
+    }
+  );
+}}
