87338e7aeb399751c39c1b58d2841f7624422c25
tasajos <tasajos@gmail.com>
2023-06-16 14:28:26 -0400

    update plan de cuentas

--- y/sqlscripts/cuentas.sql
+++ y/sqlscripts/cuentas.sql
@@ -28,3 +28,43 @@ SELECT TOP (1000)
     [FechaCreacion],
     CONCAT([totalnum], '.', [numero],' ',[nombrecuenta] ) AS [totalnivel]
 FROM [COSSMIL].[dbo].[Activocuenta3]
+
+
+
+
+/* plan de cuentas */
+
+--select activonivel from Activocuenta 
+SELECT activonivel AS Activo_N2 FROM Activocuenta;
+
+SELECT totalnivel AS Activo_N3 FROM Activocuenta3;
+
+SELECT totalnivel AS Activo_N4 FROM Activocuenta4;
+
+
+--PLan de Cuentas Activos
+
+SELECT activonivel, nombrecuenta AS Activo_N2 FROM Activocuenta
+UNION
+SELECT totalnivel, nombrecuenta AS Activo_N3 FROM Activocuenta3
+UNION
+SELECT totalnivel, nombrecuenta AS Activo_N4 FROM Activocuenta4
+UNION
+SELECT totalnivel, nombrecuenta AS Activo_N5 FROM Activocuenta5
+UNION
+SELECT totalnivel, nombrecuenta AS Activo_N6 FROM Activocuenta6
+ORDER BY activonivel;
+
+--PLan de Cuentas Pasivos
+SELECT activonivel, nombrecuenta AS Pasivo_N2 FROM Pasivocuentan2
+UNION
+SELECT totalnivel, nombrecuenta AS Activo_N3 FROM Pasivocuentan3
+UNION
+SELECT totalnivel, nombrecuenta AS Activo_N4 FROM Pasivocuentan4
+UNION
+SELECT totalnivel, nombrecuenta AS Activo_N5 FROM Pasivocuentan5
+UNION
+SELECT totalnivel, nombrecuenta AS Activo_N6 FROM Pasivocuentan6
+ORDER BY activonivel;
+
+--PLan de Cuentas Patrimonio
--- y/src/app/Components/Contabilidad/Cont/plandecuentas/ccoperacion/n6co/n6co.component.ts
+++ y/src/app/Components/Contabilidad/Cont/plandecuentas/ccoperacion/n6co/n6co.component.ts
@@ -1,10 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ContabiliadService } from 'src/app/services/contabiliad.service';
+import { HttpClient } from '@angular/common/http';
+import { COperacionIntern5, COperacionIntern6 } from 'src/app/Interfaz/contabilidad';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-n6co',
   templateUrl: './n6co.component.html',
   styleUrls: ['./n6co.component.css']
 })
-export class N6coComponent {
+export class N6coComponent implements OnInit {
+  formulario: FormGroup;
+  activos: COperacionIntern5[] = [];
+  filteredActivos: COperacionIntern5[] = []; // Lista filtrada de activos
 
-}
+  constructor(
+    private fb: FormBuilder,
+    private _snackBar: MatSnackBar,
+    private _rcontabilidadchicaService: ContabiliadService,
+    private router: Router,
+    private http: HttpClient,
+    private route: ActivatedRoute
+  ) {
+    this.formulario = this.fb.group({
+      nivel: ['', Validators.required],
+      nombrecuenta: ['', Validators.required],
+      cuentamayor: ['', Validators.required],
+      totalnum: ['', Validators.required],
+      numero: ['', Validators.required],
+         });
+  }
+
+  registrarcuenta() {
+
+     const rcactivo: COperacionIntern6 = {
+      nivel: this.formulario.value.nivel,
+      nombrecuenta: this.formulario.value.nombrecuenta,
+      cuentamayor: this.formulario.value.cuentamayor,
+      totalnum: this.formulario.value.totalnum,
+      numero: this.formulario.value.numero,
+     
+    };
+
+    // Enviamos objeto al backend
+    this._rcontabilidadchicaService.postcostooperacion6(rcactivo).subscribe(_data => {
+      this.mensajeExito('registrado');
+      location.reload();
+      this.router.navigate(['/cdistribucion6']);
+    });
+  }
+
+  mensajeExito(texto: string) {
+    this._snackBar.open(`El proceso fue realizado y ${texto} con exito`, '', {
+      duration: 2000,
+      horizontalPosition: 'right',
+    });
+  }
+
+  ngOnInit(): void {
+   // Lógica del campo
+   this.formulario.get('nivel')?.valueChanges.subscribe(nivelSeleccionado => {
+    let cuentamayor = 0;
+    switch (nivelSeleccionado) {
+           case '5':
+        cuentamayor = 4;
+        break;
+      case '6':
+        cuentamayor = 5;
+        break;
+    }
+    this.formulario.patchValue({ cuentamayor });
+
+    // Filtrar activos
+    if (cuentamayor === 2) {
+      this.filteredActivos = this.activos.filter(activo => activo.nivel === 3);
+    } else {
+      this.filteredActivos = this.activos;
+    }
+  });
+
+  this._rcontabilidadchicaService.getcostooperacion5().subscribe(
+    (activos: COperacionIntern5[]) => {
+      this.activos = activos;
+      this.filteredActivos = activos; // Inicialmente, mostrar todos los activos
+    },
+    (error) => {
+      console.error('Error al obtener los datos de getactivo():', error);
+    }
+  );
+}}
+
